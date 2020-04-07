e0e64b89ad55e963aa5a25a850b02500c69aeaa0
running727 <running727@163.com>
2020-04-07 12:00:30 +0800

    a

---
+++ 16.04/install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source ${CURRENT_DIR}/../common/common.sh
+
+[ $(id -u) != "0" ] && { ansi -n --bold --bg-red "请用 root 账户执行本脚本"; exit 1; }
+
+#MYSQL_ROOT_PASSWORD=`random_string`
+MYSQL_ROOT_PASSWORD="running727A"
+
+function init_system {
+    export LC_ALL="en_US.UTF-8"
+    echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
+    locale-gen en_US.UTF-8
+    locale-gen zh_CN.UTF-8
+
+    ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+    apt-get update
+    apt-get install -y software-properties-common
+
+    init_alias
+}
+
+function init_alias {
+    alias sudowww > /dev/null 2>&1 || {
+        echo "alias sudowww='sudo -H -u ${WWW_USER} sh -c'" >> ~/.bash_aliases
+    }
+}
+
+function init_repositories {
+    add-apt-repository -y ppa:ondrej/php
+    add-apt-repository -y ppa:nginx/stable
+    #grep -rl ppa.launchpad.net /etc/apt/sources.list.d/ | xargs sed -i 's/ppa.launchpad.net/launchpad.proxy.ustclug.org/g'
+
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
+
+    curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+    #echo 'deb https://mirrors.tuna.tsinghua.edu.cn/nodesource/deb_8.x xenial main' > /etc/apt/sources.list.d/nodesource.list
+    #echo 'deb-src https://mirrors.tuna.tsinghua.edu.cn/nodesource/deb_8.x xenial main' >> /etc/apt/sources.list.d/nodesource.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse' > /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list
+    apt-get update
+}
+
+function install_basic_softwares {
+    apt-get install -y curl git build-essential unzip supervisor
+}
+
+function install_node_yarn {
+    apt-get install -y nodejs yarn
+    sudo -H -u ${WWW_USER} sh -c 'cd ~ && yarn config set registry https://registry.npm.taobao.org'
+}
+
+function install_php {
+    apt-get install -y php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-pgsql php7.4-readline php7.4-xml php7.4-zip php7.4-sqlite3
+}
+
+function install_others {
+    apt-get remove -y apache2
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_ROOT_PASSWORD}"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_ROOT_PASSWORD}"
+    #apt-get install -y nginx mysql-server redis-server memcached beanstalkd sqlite3
+	apt-get install -y nginx mysql-server redis-server
+    chown -R ${WWW_USER}.${WWW_USER_GROUP} /var/www/
+    systemctl enable nginx.service
+}
+
+function install_composer {
+    wget https://dl.laravel-china.org/composer.phar -O /usr/local/bin/composer
+    chmod +x /usr/local/bin/composer
+    sudo -H -u ${WWW_USER} sh -c  'cd ~ && composer config -g repo.packagist composer https://packagist.laravel-china.org'
+}
+
+call_function init_system "正在初始化系统" ${LOG_PATH}
+call_function init_repositories "正在初始化软件源" ${LOG_PATH}
+call_function install_basic_softwares "正在安装基础软件" ${LOG_PATH}
+call_function install_php "正在安装 PHP" ${LOG_PATH}
+call_function install_others "正在安装 Mysql / Nginx / Redis" ${LOG_PATH}
+#call_function install_others "正在安装 Mysql / Nginx / Redis / Memcached / Beanstalkd / Sqlite3" ${LOG_PATH}
+#call_function install_node_yarn "正在安装 Nodejs / Yarn" ${LOG_PATH}
+#call_function install_composer "正在安装 Composer" ${LOG_PATH}
+
+ansi --green --bold -n "安装完毕"
+ansi --green --bold "Mysql root 密码："; ansi -n --bold --bg-yellow --black ${MYSQL_ROOT_PASSWORD}
+ansi --green --bold -n "请手动执行 source ~/.bash_aliases 使 alias 指令生效。"
